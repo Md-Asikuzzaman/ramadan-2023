c065c475c72875159139a4bb28f9717411d336d2
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2023-04-06 17:00:25 +0600

    added new data and code

--- script/iftar.js
+++ script/iftar.js
@@ -66,25 +66,67 @@ const iftarProgress = () => {
 
   const countDown = `${iftarFun.d_hour}:${iftarFun.d_minute}:${iftarFun.d_second}`;
 
-  if (iftarFun.d_hour > -1 || iftarFun.d_hour > -1 || iftarFun.d_second > -1) {
-    const start = new Date(
-      `${ramadanDate} ${currentMonth} , 2023 ${sehriLastTime}`
-    );
-    const end = new Date(`${ramadanDate} ${currentMonth}, 2023 ${iftarTime}`);
+  // checker
 
-    const today = new Date();
-    var percentage = ((today - start) * 100) / (end - start);
+  // check the specific time
+  const checkTime = (date, month, time) => {
+    console.log('TCL: checkTime -> time', time);
+    var dest = new Date(`${date} ${month}, 2023 ${time}`).getTime();
+    var now = new Date(`${date} ${month}, 2023 12:00:01 AM`).getTime();
 
-    const radius = progress.r.baseVal.value;
-    const circleRef = Math.PI * (radius * 2);
-    const point = Math.floor(circleRef - (percentage / 100) * circleRef);
+    var diff = dest - now;
+    var hour = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minute = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    var second = Math.floor((diff % (1000 * 60)) / 1000);
 
-    progress.style.strokeDashoffset = point;
-    progress.style.stroke = '#1dc01d';
-    progress.style.transition = '300ms';
+    // set countdown time
+    const d_hour = hour.toString().length == '1' ? '0' + hour : hour;
+    const d_minute = minute.toString().length == '1' ? '0' + minute : minute;
+    const d_second = second.toString().length == '1' ? '0' + second : second;
 
-    // count down display settings
-    iftar__countdown.innerHTML = countDown;
+    return {
+      d_hour,
+      d_minute,
+      d_second,
+    };
+  };
+
+  let theTime = checkTime(ramadanDate, currentMonth, sehriLastTime);
+  console.log('TCL: iftarProgress -> theTime', theTime);
+
+  if (
+    theTime.d_hour !== 0 ||
+    (theTime.d_hour < -1 && theTime.d_minute !== 0) ||
+    (theTime.d_minute < -1 && theTime.d_second !== 0) ||
+    theTime.d_second < -1
+  ) {
+    if (
+      iftarFun.d_hour > -1 ||
+      iftarFun.d_hour > -1 ||
+      iftarFun.d_second > -1
+    ) {
+      const start = new Date(
+        `${ramadanDate} ${currentMonth} , 2023 ${sehriLastTime}`
+      );
+      const end = new Date(`${ramadanDate} ${currentMonth}, 2023 ${iftarTime}`);
+
+      const today = new Date();
+      var percentage = ((today - start) * 100) / (end - start);
+
+      const radius = progress.r.baseVal.value;
+      const circleRef = Math.PI * (radius * 2);
+      const point = Math.floor(circleRef - (percentage / 100) * circleRef);
+
+      progress.style.strokeDashoffset = point;
+      progress.style.stroke = '#1dc01d';
+      progress.style.transition = '300ms';
+
+      // count down display settings
+      iftar__countdown.innerHTML = countDown;
+    } else {
+      progress.style.stroke = '#1dc01d';
+      iftar__countdown.innerHTML = 'ইফতার করুন।';
+    }
   } else {
     progress.style.stroke = '#1dc01d';
     iftar__countdown.innerHTML = 'ইফতার করুন।';
